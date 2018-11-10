d91fc56a48375342b4a7fbd5cca906b389b40ae3
odair <odair>
2018-11-10 21:50:05 -0200

    deep

--- breast_simple.py
+++ breast_simple.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-previsores = pd.read_csv("entradas_breast.csv")
-classe = pd.read_csv("saidas_breast.csv")
+previsores = pd.read_csv("entradas-breast.csv")
+classe = pd.read_csv("saidas-breast.csv")
 
 from sklearn.model_selection import train_test_split
 previsores_treinamento, previsores_teste,classe_treinamento, classe_teste = train_test_split(previsores,classe,test_size=0.25)
@@ -11,9 +11,25 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 classificador = Sequential()
-classificador.add(Dense(units = 16, activation = 'relu', 
+
+ classificador.add(Dense(units = 16, activation = 'relu', 
                         kernel_initializer= 'random_uniform', input_dim = 30))
+
+classificador.add(Dense(units = 16, activation = 'relu', 
+                        kernel_initializer= 'random_uniform'))
+
 classificador.add(Dense(units = 1, activation = 'sigmoid'))
 
-classificador.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['binary_accuracy'])
-classificador.fit(previsores_treinamento, classe_treinamento, batch_size = 10, epochs = 100)
+otimizador = keras.optimizers.Adam(lr =0.001, decay=0.0001, clipvalue=0.5)
+
+classificador.compile(optimizer = otimizador, loss = 'binary_crossentropy', metrics = ['binary_accuracy'])
+classificador.fit(previsores_treinamento, classe_treinamento,
+                  batch_size = 10, epochs = 100)
+
+previsoes = classificador.predict(previsores_teste)
+previsoes = (previsoes > 0.5)
+from sklearn.metrics import confusion_matrix, accuracy_score
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
+
+resultado = classificador.evaluate(previsores_teste, classe_teste)
